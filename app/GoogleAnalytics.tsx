aff2dc1171d9bc36b729fffc26616f63308bcbaf
WEI JUN <weijunext@gmail.com>
2024-03-06 14:16:25 +0800

    feat: Google analytics and baidu  tongji support without configuration

--- app/GoogleAnalytics.tsx
+++ app/GoogleAnalytics.tsx
@@ -6,24 +6,30 @@ import * as gtag from "../gtag.js";
 const GoogleAnalytics = () => {
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-            page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+      {gtag.GA_TRACKING_ID ? (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
--- gtag.js
+++ gtag.js
@@ -1,15 +1,15 @@
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ID
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ID || null;
 
-export const pageview = url => {
+export const pageview = (url) => {
   window.gtag("config", GA_TRACKING_ID, {
     page_path: url,
-  })
-}
+  });
+};
 
 export const event = ({ action, category, label, value }) => {
   window.gtag("event", action, {
     event_category: category,
     event_label: label,
     value: value,
-  })
-}
+  });
+};
