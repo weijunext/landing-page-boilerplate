0f86ef97733e671c05002921ad46f5e800bf7d0f
WEI JUN <weijunext@gmail.com>
2024-03-10 20:03:00 +0800

    feat: metadata uses default value

--- types/siteConfig.ts
+++ types/siteConfig.ts
@@ -25,7 +25,7 @@ export type SiteConfig = {
   headerLinks: Link[]
   footerLinks: Link[],
   footerProducts: ProductLink[]
-  metadataBase: URL
+  metadataBase: URL | string
   themeColor: string
   icons: {
     icon: string
