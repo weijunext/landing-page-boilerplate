4b344109e4fdcf1680314eb816204351c103d23b
WEI JUN <weijunext@gmail.com>
2024-03-18 16:27:50 +0800

    style: Optimize component names

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,26 @@ import CTAButton from "@/components/home/CTAButton";
 import FQA from "@/components/home/FQA";
 import Feature from "@/components/home/Feature";
 import Introduction from "@/components/home/Introduction";
+import MakerShowcase from "@/components/home/MakerShowcase";
 import Pricing from "@/components/home/Pricing";
-import UserPurchaseAvatar from "@/components/home/UserPurchaseAvatar";
 import WallOfLove from "@/components/home/WallOfLove";
 
 export default function Home() {
   return (
     <>
+      {/* Hero Section */}
       <Introduction />
-      <UserPurchaseAvatar />
+      {/* Social Proof 1 - maker showcase */}
+      <MakerShowcase />
+      {/* USP (Unique Selling Proposition) */}
       <Feature id="Feature" />
+      {/* Pricing */}
       <Pricing id="Pricing" />
+      {/* Social Proof 2 - Wall of Love */}
       <WallOfLove id="WallOfLove" />
+      {/* FAQ (Frequently Asked Questions) */}
       <FQA id="FQA" />
+      {/* CTA (Call to Action) */}
       <CTAButton />
     </>
   );
--- components/Home/MakerShowcase.tsx
+++ components/Home/MakerShowcase.tsx
@@ -1,34 +1,5 @@
 import Image from "next/image";
 
-const UserPurchaseAvatar = () => {
-  return (
-    <div className="flex flex-col items-center justify-center gap-20 mt-14">
-      <div className="flex flex-col items-center gap-5">
-        <div className="flex items-center justify-center">
-          {Users.map((user, index) => {
-            return (
-              <Image
-                key={index}
-                src={user.image}
-                alt="User"
-                height={40}
-                width={40}
-                className="rounded-full -m-[5px] border border-white"
-              />
-            );
-          })}
-        </div>
-        <p className="text-sm text-gray-600">
-          <span className="text-primary font-semibold text-base">1000+</span>{" "}
-          makers ship easier
-        </p>
-      </div>
-    </div>
-  );
-};
-
-export default UserPurchaseAvatar;
-
 const Users = [
   {
     image: "/images/users/1.png",
@@ -46,3 +17,32 @@ const Users = [
     image: "/images/users/5.png",
   },
 ];
+
+const MakerShowcase = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-20 mt-14">
+      <div className="flex flex-col items-center gap-5">
+        <div className="flex items-center justify-center">
+          {Users.map((user, index) => {
+            return (
+              <Image
+                key={index}
+                src={user.image}
+                alt="User"
+                height={40}
+                width={40}
+                className="rounded-full -m-[5px] border border-white"
+              />
+            );
+          })}
+        </div>
+        <p className="text-sm text-slate-700 dark:text-slate-400">
+          <span className="text-primary font-semibold text-base">1000+</span>{" "}
+          makers ship fast
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MakerShowcase;
