668ebd41eaaf971700196ba6bc78a4a2312ce712
WEI JUN <weijunext@gmail.com>
2024-03-19 16:27:36 +0800

    feat: FAQs

--- components/Home/FQA.tsx
+++ components/Home/FQA.tsx
@@ -1,9 +1,108 @@
+"use client";
+import { Accordion, AccordionItem } from "@nextui-org/react";
+import { PlusIcon } from "lucide-react";
+
 const FQA = ({ id }: { id: string }) => {
   return (
-    <section id={id} className="h-48 flex items-center">
-      TODO: FQA
+    <section
+      id={id}
+      className="flex flex-col justify-center max-w-[88%] items-center py-16 gap-12"
+    >
+      <div className="flex flex-col text-center max-w-xl gap-4">
+        <h2 className="text-center">FAQs</h2>
+        <p className="text-large text-default-500">
+          Here are some of the most frequently asked questions.
+        </p>
+      </div>
+      <Accordion
+        fullWidth
+        keepContentMounted
+        className="gap-3"
+        itemClasses={{
+          base: "px-6 !bg-default-100 !shadow-none hover:!bg-default-200/50",
+          title: "font-medium",
+          trigger: "py-6",
+          content: "pt-0 pb-6 text-base text-default-500",
+        }}
+        items={FAQS}
+        selectionMode="multiple"
+        variant="splitted"
+      >
+        {FAQS.map((item) => (
+          <AccordionItem
+            key={item.title}
+            indicator={<PlusIcon />}
+            title={item.title}
+          >
+            {item.content}
+          </AccordionItem>
+        ))}
+      </Accordion>
     </section>
   );
 };
 
 export default FQA;
+
+const FAQS = [
+  {
+    title: "What is a landing page?",
+    content:
+      "A landing page is a standalone web page created specifically for a marketing or advertising campaign. It's where a visitor 'lands' after they click on a link in an email, or ads from Google, Bing, YouTube, Facebook, Instagram, Twitter, or similar places on the web. Unlike web pages, which typically have many goals and encourage exploration, landing pages are designed with a single focus or goal, known as a call to action (CTA).",
+  },
+  {
+    title: "How can I create a landing page?",
+    content:
+      "Creating a landing page can be done in several steps: 1) Choose a landing page template that fits your campaign's needs or start from scratch. 2) Identify your landing page's objective and CTA. 3) Write compelling content that urges visitors to take action. 4) Incorporate design elements such as images, videos, and forms that align with your message. 5) Test your page's usability and mobile responsiveness. 6) Use SEO best practices to help your page get found. 7) Publish your landing page and monitor its performance to make adjustments as needed.",
+  },
+  {
+    title: "Why is a landing page important?",
+    content:
+      "Landing pages are crucial for any marketing strategy because they provide a targeted platform for converting visitors into leads or customers. Unlike general websites, landing pages focus the visitor's attention on a single offer, minimizing distractions and guiding them towards a specific action. This focused approach can significantly increase the effectiveness of your marketing campaigns, improve your ad performance, and enhance the overall user experience by providing them with the information or solution they were seeking.",
+  },
+  {
+    title: "Why choose our landing page boilerplate?",
+    content:
+      "Our landing page template is free, open-source, and powerful, making it ideal for various projects and marketing campaigns. It allows you to create a landing page in under an hour, simplifying the development process while also offering a high degree of customization and responsiveness.",
+  },
+  {
+    title: "What skills do I need to use this template?",
+    content:
+      "To use our landing page template, you only need basic web editing skills. The design of the template is intended to be easy for non-technical users to get started with, while also providing high customizability for experienced developers.",
+  },
+  {
+    title: "How do I get started with the landing page template?",
+    content:
+      "Getting started is very simple. Just download the source code from our GitHub repository, edit the text and images as needed, and then deploy it to your server.",
+  },
+  {
+    title: "Does the landing page template support responsive design?",
+    content:
+      "Yes, our landing page template supports responsive design, ensuring that your webpage displays perfectly on a variety of devices, whether desktop, tablet, or smartphone.",
+  },
+  {
+    title: "Can I use this template for a commercial project?",
+    content:
+      "Absolutely. Our template is open-source, and you can use it for any type of project, including commercial purposes. We encourage users to utilize this template to create and optimize their online projects.",
+  },
+  {
+    title: "Do you offer any customization services?",
+    content:
+      "We accept paid custom development of landing pages. Please contact us via email at weijunext@gmail.com or Twitter @weijunext.",
+  },
+  {
+    title: "Are there any restrictions on using this template?",
+    content:
+      "No. You are free to use and modify this template. At the same time, we encourage developers to submit PRs to help improve this landing page template together.",
+  },
+  {
+    title: "Does this template include SEO optimization features?",
+    content:
+      "Yes, our template includes basic SEO optimization features to help your webpage achieve better rankings in search engines.",
+  },
+  {
+    title: "What should I do if I encounter problems using the template?",
+    content:
+      "If you encounter any issues while using the template, please contact us via Twitter @weijunext.",
+  },
+];
