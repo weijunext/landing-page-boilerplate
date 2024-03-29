9904b44c38e629e43831b55cf59c7cf72363c050
WEI JUN <weijunext@gmail.com>
2024-03-29 16:30:59 +0800

    style: add width

--- components/home/CTA.tsx
+++ components/home/CTA.tsx
@@ -4,7 +4,7 @@ import { RoughNotation } from "react-rough-notation";
 
 const CTA = ({ locale, CTALocale }: { locale: any; CTALocale: any }) => {
   return (
-    <section className="flex flex-col justify-center max-w-[%] items-center py-16 gap-12">
+    <section className="flex flex-col justify-center max-w-[88%] items-center py-16 gap-12">
       <div className="flex flex-col text-center gap-4">
         <h2 className="text-center">{locale.title}</h2>
         <p className="text-large text-default-500">
