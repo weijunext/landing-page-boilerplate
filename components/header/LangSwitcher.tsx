ff268fd236542f9a6a3f376b2590cfa3ae4cc14f
WEI JUN <weijunext@gmail.com>
2024-09-04 14:39:46 +0800

    feat: support multiple pages under [lang] directories

--- components/header/LangSwitcher.tsx
+++ components/header/LangSwitcher.tsx
@@ -6,24 +6,36 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { useParams, useRouter } from "next/navigation";
+import { useParams, usePathname, useRouter } from "next/navigation";
 
 import { defaultLocale, localeNames } from "@/lib/i18n";
 
 export const LangSwitcher = () => {
   const params = useParams();
-  const lang = params.lang;
-
-  // const lang = (params.lang && params.lang[0]) || defaultLocale;
-  let langName = lang || defaultLocale;
+  const pathname = usePathname();
   const router = useRouter();
 
+  let langName = (params.lang as string) || defaultLocale;
+
   const handleSwitchLanguage = (value: string) => {
-    if (value === defaultLocale) {
-      router.push("/");
-      return;
+    const pathSegments = pathname
+      .split("/")
+      .filter((segment) => segment !== "");
+    let newPath;
+
+    if (pathSegments[0] === params.lang) {
+      // Current path has a language code
+      newPath = pathSegments.slice(1).join("/");
+    } else {
+      // Current path doesn't have a language code
+      newPath = pathSegments.join("/");
     }
-    router.push(value);
+
+    if (value === defaultLocale) {
+      router.push(`/${newPath}`);
+    } else {
+      router.push(`/${value}/${newPath}`);
+    }
   };
 
   return (
