299f480928a61d29324771040f3709666fdbfba8
WEI JUN <weijunext@gmail.com>
2024-09-09 23:06:03 +0800

    feat: site_url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://starter.weijunext.com",
+  siteUrl: process.env.SITE_URL || "https://landingpage.weijunext.com",
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
