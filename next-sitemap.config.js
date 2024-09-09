eafecc9cb77111c4922490ff21222ceaa04165b3
WEI JUN <weijunext@gmail.com>
2024-09-09 23:16:38 +0800

    chore: update env

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://landingpage.weijunext.com",
+  siteUrl:
+    process.env.NEXT_PUBLIC_SITE_URL || "https://landingpage.weijunext.com",
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
