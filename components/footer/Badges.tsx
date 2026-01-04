6d932d692fbf01fe0b3ff12c0d2f24bdbedc50c2
weijunext <weijunext@gmail.com>
2026-01-04 10:39:28 +0800

    chore: badge

--- components/footer/Badges.tsx
+++ components/footer/Badges.tsx
@@ -4,6 +4,18 @@ export default function Badges() {
   return (
     <div className="py-6 flex justify-center w-full flex-wrap gap-2">
       <a
+        href="https://firstlook.tools"
+        title="Featured on FirstLook.Tools"
+        target="_blank"
+      >
+        <img
+          src="https://firstlook.tools/badge/badge_light.svg"
+          alt="Featured on FirstLook.Tools"
+          width="200"
+          height="54"
+        />
+      </a>
+      <a
         href="https://dofollow.tools"
         title="Featured on Dofollow.Tools"
         target="_blank"
